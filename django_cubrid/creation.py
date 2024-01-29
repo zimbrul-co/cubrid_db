bde86ae40de0db1ecd2d8917bd0b9606adfeae8b
Casian Andrei <casian@zco.ro>
2024-01-29 20:25:16 +0200

    Rework the django_cubrid.creation module
    * implement _clone_test_db
    * reuse the commands for all the creation methods
    * simplify logic
    * test with the Django tests, with --keepdb and without
    * check if the CUBRID server is running for the target database
    * handle all situations

--- django_cubrid/creation.py
+++ django_cubrid/creation.py
@@ -29,11 +29,65 @@ Note:
 - This module is specific to the Django backend for CUBRID and may contain
   CUBRID-specific implementations and considerations.
 """
+import os
 import sys
 import time
 import subprocess
 
 from django.db.backends.base.creation import BaseDatabaseCreation
+
+
+def _database_exists(db_name):
+    """Check if a CUBRID database exists"""
+    # Read the CUBRID databases.txt file
+    db_path = os.environ['CUBRID_DATABASES']
+    db_path = os.path.join(db_path, "databases.txt")
+    with open(db_path, "r", encoding='ascii') as f:
+        db_lines = f.readlines()
+
+    # Skip the header line
+    db_lines = db_lines[1:]
+
+    # Keep only the first column, to get the list of database names
+    db_lines = list(map(lambda l: l.split()[0], db_lines))
+
+    # Return if db_name is in the list of database names
+    return db_name in db_lines
+
+def _is_server_running(db_name):
+    """Return whether the CUBRID server is running"""
+    cp = subprocess.run(["cubrid", "server", "status"],
+        capture_output = True, check = True, text = True)
+    return f'Server {db_name}' in cp.stdout
+
+def _check_consistency(db_name):
+    """Checks the consistency of a database"""
+    try:
+        subprocess.run(["cubrid", "checkdb", db_name], check = True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+def _get_create_command(db_name):
+    """Get the command to create a new CUBRID database"""
+    return ["cubrid", "createdb", "--db-volume-size=20M",
+            "--log-volume-size=20M", db_name, "en_US.utf8"]
+
+def _get_copy_command(source_db_name, dest_db_name):
+    """Get the command to create a new CUBRID database"""
+    return ["cubrid", "copydb", "--replace", source_db_name, dest_db_name]
+
+def _get_start_command(db_name):
+    """Get the command to start the CUBRID server for a given database"""
+    return ["cubrid", "server", "start", db_name]
+
+def _get_stop_command(db_name):
+    """Get the command to stop the CUBRID server for a given database"""
+    return ["cubrid", "server", "stop", db_name]
+
+def _get_delete_command(db_name):
+    """Get the command to delete a CUBRID database"""
+    return ["cubrid", "deletedb", db_name]
 
 
 class DatabaseCreation(BaseDatabaseCreation):
@@ -75,63 +129,61 @@ class DatabaseCreation(BaseDatabaseCreation):
         deletion and recreation of the test database.
         """
         test_database_name = self._get_test_db_name()
+        display_name = self._get_database_display_str(verbosity, test_database_name)
 
-        # Create the test database and start the cubrid server.
-        check_command = ["cubrid", "checkdb", test_database_name]
-        create_command = ["cubrid", "createdb" , "--db-volume-size=20M",
-                          "--log-volume-size=20M", test_database_name, "en_US.utf8"]
-        start_command = ["cubrid", "server", "start", test_database_name]
-        stop_command = ["cubrid", "server", "stop", test_database_name]
-        delete_command = ["cubrid", "deletedb", test_database_name]
+        # Check if the test database already exists
+        database_exists = _database_exists(test_database_name)
+        self.log(f"Database exists for alias {display_name}: {database_exists}")
 
-        if keepdb:
-            # Check if the test database already exists
-            try:
-                subprocess.run(check_command, check = True)
-                print("Database already exists")
-                return test_database_name
-            except subprocess.CalledProcessError:
-                pass
+        # Check whether the CUBRID server is started for this database
+        is_server_running = _is_server_running(test_database_name) if database_exists else False
+        self.log(f"Database server running for alias {display_name}: {is_server_running}")
 
-        try:
-            cp = subprocess.run(create_command, capture_output = True, check = False)
-            self.log(cp.stdout.decode())
-            self.log(cp.stderr.decode())
+        def create_db():
+            self.log(f"Creating new database: {test_database_name}")
+            cp = subprocess.run(_get_create_command(test_database_name),
+                capture_output = True, check = False, text = True)
+            self.log(cp.stdout)
+            self.log(cp.stderr)
             cp.check_returncode()
-            print('Created')
-            subprocess.run(start_command, check = True)
-            print('Started')
-            subprocess.run(check_command, check = True)
-            self.connection.cursor()
-            print('Checked')
 
-        except subprocess.CalledProcessError as e:
-            self.log(f"Error creating the test database: {e}")
+        def destroy_db():
             if not autoclobber:
                 confirm = input(f"Type 'yes' if you would like to try deleting the test "
-                                f"'database '{test_database_name}', or 'no' to cancel: ")
+                                f"database '{test_database_name}', or 'no' to cancel: ")
             if autoclobber or confirm == 'yes':
-                try:
-                    if verbosity >= 1:
-                        print("Destroying old test database...")
-                        subprocess.run(stop_command, check = True)
-                        subprocess.run(delete_command, check = True)
-
-                        print("Creating test database...")
-                        cp = subprocess.run(create_command, capture_output = True, check = False)
-                        self.log(cp.stdout.decode())
-                        self.log(cp.stderr.decode())
-                        cp.check_returncode()
-                        print('Created')
-
-                        subprocess.run(start_command, check = True)
-                        print('Started')
-                except subprocess.CalledProcessError as e2:
-                    self.log(f"Error recreating the test database: {e2}")
-                    sys.exit(2)
+                self.log(f"Destroying old database for alias {display_name}: {test_database_name}")
+                if is_server_running:
+                    subprocess.run(_get_stop_command(test_database_name), check = True)
+                subprocess.run(_get_delete_command(test_database_name), check = True)
             else:
-                print( "Tests cancelled.")
+                self.log("Tests cancelled.")
                 sys.exit(1)
+
+        def start_server():
+            self.log("Starting the CUBRID server")
+            subprocess.run(_get_start_command(test_database_name), check = True)
+
+        if database_exists:
+            if keepdb:
+                if not is_server_running:
+                    start_server()
+                    is_server_running = True
+
+                # Check the consistency of the database
+                if _check_consistency(test_database_name):
+                    return test_database_name
+
+                self.log("Database consistency check failed")
+                destroy_db()
+            else:
+                self.log(f"Cannot use old database for alias {display_name}")
+                destroy_db()
+
+        # The database does not exist, or the old one has been destroyed
+        # Create a new test database and start the CUBRID server for it
+        create_db()
+        start_server()
 
         return test_database_name
 
@@ -153,9 +205,51 @@ class DatabaseCreation(BaseDatabaseCreation):
         """
         time.sleep(1) # To avoid "database is being accessed by other users" errors.
         try:
-            subprocess.run(["cubrid", "server", "stop", test_database_name], check = True)
-            subprocess.run(["cubrid", "deletedb", test_database_name], check = True)
+            subprocess.run(_get_stop_command(test_database_name), check = True)
+            subprocess.run(_get_delete_command(test_database_name), check = True)
         except subprocess.CalledProcessError as e:
             self.log(f"Error destroying the test database: {e}")
         finally:
             self.connection.close()
+
+    def _clone_test_db(self, suffix, verbosity, keepdb=False):
+        source_database_name = self.connection.settings_dict["NAME"]
+        target_database_name = self.get_test_db_clone_settings(suffix)["NAME"]
+        display_name = self._get_database_display_str(verbosity, source_database_name)
+
+        # Check if the target database already exists
+        database_exists = _database_exists(target_database_name)
+
+        # Check whether the CUBRID server is started for this database
+        is_server_running = _is_server_running(target_database_name) if database_exists else False
+
+        def copy_db():
+            self.log(f"Cloning database for alias {display_name} to {target_database_name}")
+            cp = subprocess.run(_get_copy_command(source_database_name, target_database_name),
+                check = False, capture_output = True, text = True)
+            self.log(cp.stdout)
+            self.log(cp.stderr)
+            cp.check_returncode()
+
+        if database_exists:
+            if keepdb:
+                if not is_server_running:
+                    subprocess.run(_get_start_command(target_database_name), check = True)
+                    is_server_running = True
+
+                # Check the consistency of the database
+                if _check_consistency(target_database_name):
+                    return
+
+                self.log(f"Database consistency check failed for alias {display_name}")
+
+        # The CUBRID servers need to be stopped for copy_db to be possible
+        subprocess.run(_get_stop_command(source_database_name), check = False)
+        if is_server_running:
+            subprocess.run(_get_stop_command(target_database_name), check = True)
+
+        # The target database does not exist, or the old one has been destroyed
+        # Clone the existing database and restart the CUBRID server for both
+        copy_db()
+        subprocess.run(_get_start_command(source_database_name), check = True)
+        subprocess.run(_get_start_command(target_database_name), check = True)
